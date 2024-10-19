de3c20068188f51c8c33b5d1d9234e037d7cb9fa
bongmin25 <tom.dantas25@gmail.com>
2024-10-19 14:54:40 -0300

    Agegando peticion de cursos del back

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,23 +1,18 @@
-import Card from '@/components/Card/Card';
-import CardList from '@/components/CardList/CardList';
-import { course } from '../../mock/products';
-import style from './page.module.css';
-import { ICourse } from '@/interfaces/Icourse';
-import { getProducts } from '@/services/productsServices';
+import Card from "@/components/Card/Card";
+import CardList from "@/components/CardList/CardList";
+import style from "./page.module.css";
+import { ICourse } from "@/interfaces/Icourse";
+import { getCourse } from "@/services/productsServices";
 
 const page = async () => {
-  //const url = process.env.API_URL + '/products';
-  // const response = await fetch(url, { next: { revalidate: 0 } });
-  // const products: IProduct[] = await response.json();  
-  //const products = await getProducts(url);
+  const url = `${process.env.API_URL}/courses`; 
+  const courses = await getCourse(url); 
+
   return (
-    <main className="!mt-24 mb-10">
+    <main className={`container ${style.container}`}>
       <CardList>
-        {course.map((product: ICourse, i: number) => (
-          <Card
-            key={i}
-            course={product}
-          />
+        {courses.map((course: ICourse, i: number) => (
+          <Card key={i} course={course} />
         ))}
       </CardList>
     </main>
@@ -25,5 +20,3 @@ const page = async () => {
 };
 
 export default page;
-
-/* className={`container ${style.container}`} */
