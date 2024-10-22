a306b4ee7f794993b473d379ca9ea755bd115d64
DamiCode <cdamiang90@gmail.com>
2024-10-22 18:56:04 -0300

    diseño responsive page study

--- src/app/study/page.tsx
+++ src/app/study/page.tsx
@@ -35,13 +35,13 @@ const Study: React.FC = () => {
   ];
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
+    <div className="grid grid-cols-1 md:flex md:flex-col items-center justify-center min-h-screen bg-tertiary p-6">
       <h1 className="text-3xl font-bold mb-6 text-center">Curso PHP - DevNavigator</h1>
-      <div className="max-w-4xl w-full flex">
+      <div className="max-w-4xl w-full  md:flex">
         <div className="flex-1 flex justify-center">
           {activeVideo !== null && (
             <iframe
-              className="rounded mb-4"
+              className="rounded-2xl mb-4"
               width="100%"
               height="315"
               src={videoUrls[activeVideo]}
@@ -54,7 +54,7 @@ const Study: React.FC = () => {
         </div>
 
     
-        <div className="border-l-2 border-blue-500 mx-4" style={{ height: '315px' }}></div>
+        <div className="border mb-4 md:border-l-2 border-blue-500 mx-4 md:h-[315px]" ></div>
 
         <div className="flex flex-col ml-4"> 
           {videoDescriptions.map((description, index) => (
