a9684db8c5a5856d8f279b92b0a451be80086da3
John Di Donna <139842867+DiDonna29@users.noreply.github.com>
2024-11-13 01:03:29 -0400

    implementacion 2.0 de reestablecimiento de contraseña

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,27 +1,47 @@
-import Button from '@/components/Button/Button';
-import ButtonGoogle from '@/components/ButtonGoogle/ButtonGoogle';
-import LoginForm from '@/components/LoginForm/LoginForm';
-import { signIn, useSession } from 'next-auth/react';
+// page.tsx
 
-// import { LoginButton } from "@/components/LoginButton/LoginButton";
+"use client"; // Añade esta línea al principio del archivo
 
-const page = () => {
+import Button from "@/components/Button/Button";
+import ButtonGoogle from "@/components/ButtonGoogle/ButtonGoogle";
+import LoginForm from "@/components/LoginForm/LoginForm";
+import ResetPasswordForm from "@/components/ResetPasswordForm/ResetPasswordForm";
+import { useState } from "react";
+
+const Page = () => {
+  const [showResetPassword, setShowResetPassword] = useState(false);
+
   return (
     <div className={`container !mt-24`}>
-      <div className=" w-[400px] mx-auto p-10 rounded-3xl shadow-lg shadow-gray-700/40">
-        <h1 className="text-center">Inicia sesión</h1>
-        <LoginForm />
-        <div className="mx-auto text-center mt-7">
-          <p className="mb-3">¿No tienes cuenta?</p>
-          <Button href="/register">Registrate</Button>
-        </div>
-        <div className="mx-auto text-center mt-3 flex flex-col items-center">
-          <h3 className="text-center">--------- ó ---------</h3>
-          <ButtonGoogle />
-        </div>
+      <div className="w-[400px] mx-auto p-10 rounded-3xl shadow-lg shadow-gray-700/40">
+        <h1 className="text-center">
+          {showResetPassword ? "Restablecer Contraseña" : "Inicia sesión"}
+        </h1>
+        {showResetPassword ? (
+          <ResetPasswordForm />
+        ) : (
+          <>
+            <LoginForm />
+            <div className="mx-auto text-center mt-7">
+              <p className="mb-3">¿No tienes cuenta?</p>
+              <Button href="/register">Regístrate</Button>
+            </div>
+            <div className="mx-auto text-center mt-3 flex flex-col items-center">
+              <h3 className="text-center">--------- ó ---------</h3>
+              <ButtonGoogle />
+            </div>
+            <div className="text-center mt-4">
+              <button
+                onClick={() => setShowResetPassword(true)}
+                className="text-blue-500">
+                ¿Olvidaste tu contraseña?
+              </button>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
 };
 
-export default page;
+export default Page;
